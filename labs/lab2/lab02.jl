414894c19f548979bb76e61a468afd6b1020250d
zschora <yegorssh@gmail.com>
2023-02-17 15:28:19 +0300

    feat(main): add lab2

--- labs/lab2/lab02.jl
+++ labs/lab2/lab02.jl
@@ -1,24 +1,57 @@
+# Решаем ОДУ
+
+using DifferentialEquations
 using Plots
 
-F(x) = sin(x) + cos(2x) + 0.5cos(x)
+"""Правая часть ОДУ
+u --- переменная (скаляр или массив)
+p --- параметры (кортеж, tuple)
+t --- аргумент (скаляр, время)
+"""
+function F(u, p, t)
+  return u / √(3.7^2-1)
+end
 
-x = collect(1:0.01:4pi)
-y = F.(x)
+
+"Начальное значение"
+const u_0 = 11.7/4.7
+
+"Интервал (кортеж, tuple)"
+const T = (0, 1.8π)
+
+# Задача
+prob = ODEProblem(F, u_0, T)
+
+# Решение задачи
+sol = solve(
+  prob,
+  dtmax=0.05)
 
 plt = plot(
-	aspect_ratio=:equal,
-	dpi=300,
-	lagand=false
-)
+  proj = :polar,
+  aspect_ratio=:equal,
+  dpi=300,
+  legend=true)
 
+# воскл. знак в названии обязателен 
+# в данном случае! 
 plot!(
-	plt,
-	x,
-	y,
-	xlabel="x",
-	ylabel="y",
-	color=:red,
-	title="Example"
-)
+  plt,
+  sol.t,
+  sol.u,
+  xlabel="θ",
+  ylabel="r(t)",
+  label="Траектория катера",
+  color=:blue,
+  title="Катер с бандитами")
 
-savefig(plt, "lab2.png")
+
+  plot!(
+    plt,
+    [1, 1]*19/25*pi,
+    [0, 10],
+    label="Траектория лодки",
+  )
+  
+
+savefig(plt, "lab03.1.png")
